eb8314215c47545a51547f0a780da91ed6a31091
Toader, Robert <toader.robert.cntv@gmail.com>
2024-04-19 13:05:59 +0300

    slightly change addEntry

--- src/main/java/com/toader/smarteo/bean/SmarteoResponse.java
+++ src/main/java/com/toader/smarteo/bean/SmarteoResponse.java
@@ -1,8 +1,13 @@
 package com.toader.smarteo.bean;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
+@AllArgsConstructor
 public class SmarteoResponse {
+    private Entry entry;
     private String returnMessage;
 }
--- src/main/java/com/toader/smarteo/controller/SmarteoController.java
+++ src/main/java/com/toader/smarteo/controller/SmarteoController.java
@@ -12,9 +12,7 @@ public class SmarteoController {
 
     @PostMapping("/addEntry")
     public SmarteoResponse addEntry(@RequestBody Entry entry, HttpServletRequest request) {
-        SmarteoResponse smarteoResponse = new SmarteoResponse();
-        smarteoResponse.setReturnMessage("You have succesfully added an entry!");
-        return smarteoResponse;
+        return SmarteoResponse.builder().entry(entry).returnMessage("You have successfully added an entry!").build();
     }
 
     @GetMapping("/helloWorld")
